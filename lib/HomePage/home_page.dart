bd337151a9b49a9ea89279993cbbb7c45e2598cd
Lenovo <bekhruz.khoshimov@gmail.com>
2022-12-17 19:50:57 +0500

    Initial commit

---
+++ lib/HomePage/home_page.dart
@@ -0,0 +1,197 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  static const String PLAYER_X = "X";
+  static const String PLAYER_Y = "O";
+
+  late String currentPlayer;
+  late bool gameEnd;
+  late List<String> occupied;
+
+  @override
+  void initState() {
+    initializeGame();
+    super.initState();
+  }
+
+  void initializeGame() {
+    currentPlayer = PLAYER_X;
+    gameEnd = false;
+    occupied = ["", "", "", "", "", "", "", "", ""]; //9 empty places
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _headerText(),
+            _gameContainer(),
+            _restartButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _headerText() {
+    return Column(
+      children: [
+        const Text(
+          "Tic Tac Toe",
+          style: TextStyle(
+            color: Colors.green,
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          "$currentPlayer navbat",
+          style: const TextStyle(
+            color: Color(0xCA071623),
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _gameContainer() {
+    return Container(
+      height: MediaQuery.of(context).size.height / 2,
+      width: MediaQuery.of(context).size.height / 2,
+
+      margin: const EdgeInsets.all(8),
+      child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3),
+          itemCount: 9,
+          itemBuilder: (context, int index) {
+            return _box(index);
+          }),
+    );
+  }
+
+  Widget _box(int index) {
+    return InkWell(
+      onTap: () {
+        if (gameEnd || occupied[index].isNotEmpty) {
+          return;
+        }
+
+        setState(() {
+          occupied[index] = currentPlayer;
+          changeTurn();
+          checkForWinner();
+          checkForDraw();
+        });
+      },
+      child: ClipOval(
+        child: Container(
+
+          color: occupied[index].isEmpty
+              ? Color(0x2175F391)
+              : occupied[index] == PLAYER_X
+              ? Color(0xFF2FC4D5)
+              : Color(0xFFF1ED02),
+          margin: const EdgeInsets.all(0),
+          child: Center(
+            child: Text(
+              occupied[index],
+              style: const TextStyle(fontSize: 70),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _restartButton() {
+    return ElevatedButton(
+        onPressed: () {
+
+          setState(() {
+            initializeGame();
+          });
+        },
+        child: const Text("O'yinni qayta boshlash"));
+  }
+
+  changeTurn() {
+    if (currentPlayer == PLAYER_X) {
+      currentPlayer = PLAYER_Y;
+    } else {
+      currentPlayer = PLAYER_X;
+    }
+  }
+
+  checkForWinner() {
+    //Define winning positions
+    List<List<int>> winningList = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (var winningPos in winningList) {
+      String playerPosition0 = occupied[winningPos[0]];
+      String playerPosition1 = occupied[winningPos[1]];
+      String playerPosition2 = occupied[winningPos[2]];
+
+      if (playerPosition0.isNotEmpty) {
+        if (playerPosition0 == playerPosition1 &&
+            playerPosition0 == playerPosition2) {
+          //all equal means player won
+          showGameOverMessage("$playerPosition0 O'yinchi yutdi!");
+          gameEnd = true;
+          return;
+        }
+      }
+    }
+  }
+
+  checkForDraw() {
+    if (gameEnd) {
+      return;
+    }
+    bool draw = true;
+    for (var occupiedPlayer in occupied) {
+      if (occupiedPlayer.isEmpty) {
+        draw = false;
+      }
+    }
+
+    if (draw) {
+      showGameOverMessage("Hisob Teng!");
+      gameEnd = true;
+    }
+  }
+
+  showGameOverMessage(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+          backgroundColor: Colors.green,
+          content: Text(
+            "O'yin Tugadi \n $message",
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 20,
+            ),
+          )),
+    );
+  }
+}
